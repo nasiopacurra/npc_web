62bb02df1b2214da4a9c658b7ec2dec7bc965931
nasiopacurra <38850349+nasiopacurra@users.noreply.github.com>
2018-04-30 10:07:33 +0200

    eliminar password

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,6 @@
 <?php
 $dbhost="localhost"; 	// host del MySQL 
 $dbusuario="nasio_npc"; 	// nombre de usuario
-$dbpassword="cristina";   // password de acceso
+$dbpassword="##BASICA##";   // password de acceso
 $db="nasiopacurra_wp1";        	// Seleccionamos la base con la cual trabajar
 ?>
