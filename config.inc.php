c5acf7aae103eb3cf5c4d9c1513f1b68f4be8512
amasa3w <amateos@mutua.es>
2018-04-30 12:50:27 +0200

    Limpieza por reinstalacion en directorio

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,6 @@
 <?php
 $dbhost="localhost"; 	// host del MySQL 
-$dbusuario="nasio_npc"; 	// nombre de usuario
-$dbpassword="##BASICA##";   // password de acceso
-$db="nasiopacurra_wp1";        	// Seleccionamos la base con la cual trabajar
+$dbusuario="amateos_npc"; 	// nombre de usuario
+$dbpassword="Cristina_1_npc";   // password de acceso
+$db="nasiopacurra_npc";        	// Seleccionamos la base con la cual trabajar
 ?>
